ced63bf744787a83ed05d375f0655a6891b802cb
Celeste Layne <layne.celeste@gmail.com>
2023-11-15 14:29:28 -0500

    starter code

---
+++ exercises/week-11/sentence-generator/main.js
@@ -0,0 +1,24 @@
+console.log('this works')
+
+let myElement = document.getElementById('special')
+console.log(myElement)
+
+myElement.style.color = "red";
+myElement.innerText = "This is Tuesday"
+
+// let noun = "apple";
+// let verb = "peek";
+// let adjective = "generously";
+
+// let sentence = `My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`
+// console.log(sentence)
+
+// let nouns = ["heart", "ocean", "mountain"]; // array
+// let verbs = ["look", "make", "continue"];
+// let adjectives = ["good", "different", "possible"];
+
+// let noun = nouns[Math.floor(Math.random() * nouns.length)]
+// let verb = verbs[Math.floor(Math.random() * verbs.length)]
+// let adjective = adjectives[Math.floor(Math.random() * adjectives.length)]
+
+// console.log(`My ${noun} leaps ${adjective} when I ${verb} a rainbow in the sky.`)
